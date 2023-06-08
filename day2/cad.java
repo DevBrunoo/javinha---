395b7243e1d7d6b8b359120bd31de065971b8bb5
Bruno Oliveira <96549400+DevBrunoo@users.noreply.github.com>
2023-06-07 21:51:44 -0300

    Update cad.java

---
+++ day2/cad.java
@@ -0,0 +1,330 @@
+No Alice, onde é possível ver a lista de funções disponíveis para um objeto?
+Guia Functions no painel methods. (*)
+Menu pull-down Instance.
+Guia Properties no painel methods.
+Descrição da classe no editor de cenas.
+
+
+--
+
+. No Alice, as funções são arrastadas para a instrução de controle, não para o procedimento. Verdadeiro ou falso?
+Verdade
+Falso (*)
+
+--
+
+. No Alice, depois de adicionar uma instância à cena, não é possível excluí-la. Verdadeiro ou falso?
+Verdade
+Falso (*)
+Correto
+
+--
+
+4. Qual dos seguintes itens é a instrução de controle padrão em myFirstMethod?
+Do Together
+Comment
+Count
+Do In Order (*)
+Correto
+
+---
+
+. O editor de cenas do Alice contém ferramentas para girar a visualização da câmera. Verdadeiro ou falso?
+Verdade (*)
+Falso
+Correto
+
+--
+
+. Qual dos itens a seguir é um motivo pelo qual você pode desativar instruções de programação no código do Alice?
+Para ajudar a isolar partes do código durante o teste. (*)
+Para desativar o programa inteiro.
+Para fazer com que um objeto vire e se mova simultaneamente.
+Para fazer com que um objeto se mova para frente.
+
+--
+
+De suas lições sobre o Alice, o que é método de procedimento one-shot?
+Procedimento que é invocado quando o botão Run é clicado.
+Procedimento que é usado para fazer um ajuste na cena. (*)
+Procedimento que é arrastado para o editor de código.
+Procedimento que é usado para iniciar o programa.
+Correto
+
+--
+
+Manipular manualmente um objeto do Alice com o cursor é uma forma de posicioná-lo com precisão. Verdadeiro ou falso?
+Verdade
+Falso (*)
+
+--
+
+No Alice, ao usar a função getDistanceTo qual opção do menu você usa para subtrair um valor definido da distância?
+Custom DecimalNumber
+Whole to decimal number
+Random
+Math (*)
+
+--
+
+No Alice, onde você obteria acesso a junções específicas de um objeto que não estão disponíveis pelo menu drop down de objetos?
+guia functions (*)
+guia procedures
+editor de cenas
+editor de código
+
+--
+
+. O procedimento say no Alice executa um arquivo de áudio. Verdadeiro ou falso?
+Verdade
+Falso (*)
+
+--
+
+. No Alice, cada classe tem um conjunto de procedimentos e funções predefinidos. Verdadeiro ou falso?
+Verdade (*)
+Falso
+
+--
+
+. Qual é a primeira etapa para fazer comentários em um programa do Alice?
+Arrastar e soltar o bloco de comentários abaixo de um segmento de código.
+Arrastar e soltar o bloco de comentários acima de um segmento de código. (*)
+Digitar comentários que descrevam a sequência das ações no segmento de código.
+Selecionar a instância no menu de instâncias.
+
+--
+
+ No Alice, quais das seguintes situações podem se beneficiar da declaração de um novo procedimento?
+(Escolha todas as respostas corretas)
+Um objeto precisa se mover para frente e, em seguida, se mover 10 metros para cima.
+Um objeto não tem um procedimento padrão para um movimento, como nadar. (*)
+Um único movimento, como andar, ocupa muito espaço em myFirstMethod. (*)
+Um objeto precisa fazer três afirmações.
+Vários objetos precisam usar um movimento, como coelhos pulando. (*)
+
+--
+
+ No Alice, os objetos herdam as características de:
+Seu código
+Sua classe (*)
+Seu projeto
+Seu programa
+Correto
+(1/1) Pontos
+8. Com base nas lições sobre o Alice, qual item a seguir é uma ferramenta que mostra a lógica de uma animação?
+Gráfico de classe
+Fluxograma (*)
+Storyboard visual
+Torta de pizza
+Editor de cenas
+Correto
+(1/1) Pontos
+9. O Alice 3 lembrará você periodicamente de salvar seu projeto. Verdadeiro ou falso?
+Verdade
+Falso (*)
+Correto
+(1/1) Pontos
+10. Quais dos itens a seguir são formas de abrir um arquivo de projeto existente do Alice depois de iniciar o Alice?
+(Escolha todas as respostas corretas)
+Clique duas vezes no nome do arquivo do projeto na pasta que está armazenada no computador.
+Procure o projeto usando a guia File System. (*)
+Clique e arraste o arquivo de seu computador para o Alice 3.
+Selecione o projeto na guia My Projects. (*)
+
+--
+
+11. No Alice, no que difere um procedimento one-shot dos procedimentos no editor de código?
+Os procedimentos one-shot só estão disponíveis para objetos de ação, e os procedimentos while estão disponíveis para todos os objetos.
+Os procedimentos one-shot estão disponíveis no editor de código, e os procedimentos while estão disponíveis no editor de cenas.
+Um procedimento one-shot é executado somente uma vez para reposicionar o objeto, e os procedimentos while no editor de código são executados toda vez que o botão Run é clicado. (*)
+Todas as opções acima
+Correto
+(1/1) Pontos
+12. No Alice, Do In Order e Do Together:
+São instruções move
+São instruções de controle (*)
+São instruções complexas
+Nenhuma das opções acima
+Correto
+(1/1) Pontos
+13. No Alice, qual procedimento é usado para atribuir um objeto como veículo de outro?
+setClassVehicle
+setObjectVehicle
+Veículo
+setVehicle (*)
+
+(1/1) Pontos
+14. Com base em suas lições sobre o Alice, quais dos itens a seguir são motivos para que números aleatórios sejam usados por um computador?
+(Escolha todas as respostas corretas)
+Para abrir um projeto.
+Para simulação. (*)
+Para criar procedimentos.
+Para segurança. (*)
+
+15. No Alice, qual dos argumentos a seguir poderia ser substituído por um número aleatório?
+
+Nome de objeto
+Distância (*)
+Direção
+Duração (*)
+Nome do procedimento
+
+---
+
+
+
+--
+
+9. Antes de começar a desenvolver o storyboard da animação, o que deve ser definido?
+O código
+O cenário (*)
+O processo de depuração
+As instruções de controle
+Correto
+
+--
+
+10. No Alice, você pode identificar quando novos procedimentos precisam ser declarados revisando o storyboard textual da animação. Verdadeiro ou falso?
+Verdade (*)
+Falso
+
+--
+
+. Qual ferramenta do Alice é usada para demonstrar o fluxo de processo de uma animação?
+Storyboard textual
+Torta de pizza
+Fluxograma (*)
+Mundo
+Storyboard visual
+
+--
+
+
+
+Com base nas lições sobre o Alice, números aleatórios são números gerados pelo usuário com um padrão em sua sequência. Verdadeiro ou falso?
+Verdade
+Falso (*)
+
+
+--
+
+. Qual tarefa de execução do Alice corresponde à seguinte instrução do storyboard?
+O gato rola para a esquerda.
+this.Cat roll Left 1.0 (*)
+Cat roll Right 1
+roll Left 1
+Cat roll Left 1
+
+--
+
+
+ No Alice, um programa de computador exige que funções o informem como executar o procedimento. Verdadeiro ou falso?
+Verdade
+Falso (*)
+
+--
+
+ Qual das ações a seguir exigiria uma instrução de controle para controlar o tempo de animação?
+(Escolha todas as respostas corretas)
+Um objeto rocha virando.
+Um objeto bípede caminhando. (*)
+Um peixe nadando. (*)
+Um pássaro voando. (*)
+
+
+--
+
+1. Com base nas lições sobre o Alice, números aleatórios são números gerados pelo usuário com um padrão em sua sequência. Verdadeiro ou falso?
+Verdade
+Falso (*)
+
+
+--
+
+. Com base nas lições sobre o Alice, os números aleatórios são definidos nos argumentos de distância e duração em um procedimento. Verdadeiro ou falso?
+Verdade (*)
+Falso
+
+--
+
+No Alice, qual função é usada para mover um objeto diretamente para o ponto central de outro objeto?
+getDistance (*)
+getDuration
+getDepth
+getObject
+
+--
+
+
+. No Alice, onde é possível ver a lista de funções disponíveis para um objeto?
+Menu pull-down Instance.
+Guia Properties no painel methods.
+Guia Functions no painel methods. (*)
+Descrição da classe no editor de cenas.
+
+--
+
+No Alice, você examina o código no qual um pássaro move as asas para frente e para trás enquanto se movimenta simultaneamente para a frente pela cena. Você nota que esse conjunto de procedimentos é repetido no editor de código 10 vezes para conseguir esse movimento. Como a abstração de procedimento pode ser usada para simplificar o código e facilitar a leitura?
+(Escolha todas as respostas corretas)
+Não fazer nenhuma alteração no código.
+Declarar um procedimento "fly" separado para o corpo se mover para frente e as asas para cima e para baixo. (*)
+Usar a instrução de controle Count para executar o movimento do corpo para frente e para cima e para baixo das asas 10 vezes. (*)
+Usar o editor de cenas para posicionar as asas de modo que elas subam enquanto o corpo se move para frente.
+
+--
+
+Com base nas lições sobre o Alice, qual item a seguir é uma ferramenta que mostra a lógica de uma animação?
+Storyboard visual
+Gráfico de classe
+Torta de pizza
+Editor de cenas
+Fluxograma (*)
+
+--
+
+ No Alice, novos procedimentos são declarados no editor de cenas. Verdadeiro ou falso?
+Verdade
+Falso (*)
+
+--
+
+ Os objetos do Alice se movem em relação à orientação da pessoa que está vendo a animação. Verdadeiro ou falso?
+Verdade
+Falso (*)
+
+--
+
+No Alice, o que o estilo de alça de redimensionamento faz?
+Gira ao redor dos eixos x, y, e z
+Rotação e movimentação simples
+Move ao longo dos eixos x, y, e z
+Altera o tamanho do objeto e o expande ao longo dos eixos x, y e z (*)
+
+
+--
+
+ O Alice 3 lembrará você periodicamente de salvar seu projeto. Verdadeiro ou falso?
+Verdade
+Falso (*)
+
+--
+
+Argumento é um valor que o procedimento usa para concluir sua tarefa. Verdadeiro ou falso?
+Verdade (*)
+Falso
+
+--
+
+No Alice a guia Functions exibirá as funções predefinidas para a instância selecionada. Verdadeiro ou falso?
+Verdade (*)
+Falso
+
+--
+
+
+
+
+
+
+
