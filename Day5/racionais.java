5eea3790227bc6792ac51be4e549ccbdcf04869c
Bruno Oliveira <96549400+DevBrunoo@users.noreply.github.com>
2023-08-08 00:57:56 +0000

    Da ponte pra ca

---
+++ Day5/racionais.java
@@ -0,0 +1,29 @@
+public class IncrementDecrementExample {
+    public static void main(String[] args) {
+        int pressa = 0;     // Variable to represent "pressa"
+        int fraco = 5;      // Variable to represent "fraco"
+        int covarde = 10;   // Variable to represent "covarde"
+        
+        // Increment operators (++): Increases the value by 1
+        pressa++;           // pressa = 1
+        
+        // Decrement operators (--): Decreases the value by 1
+        fraco--;            // fraco = 4
+        covarde--;          // covarde = 9
+        
+        // Post-increment operators: The current value is used before the increment
+        int vitória = pressa++;  // vitória = 1, pressa = 2
+        
+        // Pre-decrement operators: The decrement happens before the value is used
+        int espaço = --fraco;    // espaço = 3, fraco = 3
+        
+        // Compound assignment operators (+=, -=): Modify and assign values
+        fraco += 2;             // fraco = 5 + 2 = 7
+        covarde -= 3;           // covarde = 9 - 3 = 6
+        
+        // Using the variables and phrases in output
+        System.out.println("A pressa é a inimiga da " + (vitória == 1 ? "vitória" : "derrota") + ".");
+        System.out.println("O " + (fraco <= 5 ? "fraco" : "forte") + " não tem espaço.");
+        System.out.println("O " + (covarde >= 10 ? "covarde" : "valente") + " morre sem tentar!");
+    }
+}
